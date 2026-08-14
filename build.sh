25b70067ee02c795d4554b750fec1f0cf091d5ba
korbykob <korbykobs@gmail.com>
2026-08-14 18:10:03 +0100

    Allow compilation from other architectures

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 
 mkdir -p bin
-gcc $PROGRAM_COMPILER_FLAGS -isystem sys -std=gnu17 -Wall -Wextra -Werror -Wno-unused-parameter src/doom.c -o bin/doom.o
-ld $PROGRAM_LINKER_FLAGS bin/doom.o -o bin/doom.nxe
+x86_64-linux-gnu-gcc $PROGRAM_COMPILER_FLAGS -isystem sys -std=gnu17 -Wall -Wextra -Werror -Wno-unused-parameter src/doom.c -o bin/doom.o
+x86_64-linux-gnu-ld $PROGRAM_LINKER_FLAGS bin/doom.o -o bin/doom.nxe
